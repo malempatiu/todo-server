6bb3864f1939ca22eb6817a4cf140b920690cfa9
malempatiu <malempatiu@gmail.com>
2018-08-22 18:47:51 +0200

    Added user model

---
+++ models/user-model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose'),
+    validator = require('validtor');
+
+//User Schema
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 1
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+});
+
+//User Model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
