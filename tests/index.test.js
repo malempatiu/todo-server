a022d39e8daf07e0f567392029d0a4d2521a11f3
malempatiu <malempatiu@gmail.com>
2018-08-23 17:57:17 +0200

    added test cases for todo's toutes

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,160 +3,105 @@ const expect = require('expect'),
     { ObjectID } = require('mongodb');
 
 const app = require('../index'),
-    ToDoDB = require('../models/todo-model');
+    ToDoDB = require('../models/todo-model'),
+    User = require('../models/user-model');
 
-const seedToDos = [
-    {
-        _id: new ObjectID(),
-        text: "Telugu"
-    },
-    {
-        _id: new ObjectID(),
-        text: "German"
-    }]
-beforeEach((done) => {
-    ToDoDB.remove({})
-        .then(() => {
-            return ToDoDB.insertMany(seedToDos);
-        })
-        .then(() => {
-            done();
-        });
-});
+const userID = "5b7ec61bdfe1d02eb401c7a9";
+let todoID;
 
-describe('POST /api/todos', () => {
+describe('POST /api/user/:id/todos', () => {
     it('should create a new todo', (done) => {
-        const text = 'English';
+        const text = 'Testing with mocha';
         request(app)
-            .post('/api/todos')
+            .post(`/api/user/${userID}/todos`)
             .send({ text })
             .expect(200)
             .expect((res) => {
                 expect(res.body.todo.text).toBe(text);
             })
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                }
-                ToDoDB.find({ text }).then((todos) => {
-                    expect(todos.length).toBe(1);
-                    expect(todos[0].text).toBe(text);
-                    done();
-                })
-                    .catch((err) => done(err));
-            });
-    });
+            .end(done);
+    }).timeout(3000);
 
     it('should not create a new todo with invalid body', (done) => {
         request(app)
-            .post('/api/todos')
+            .post(`/api/user/${userID}/todos`)
             .send({})
             .expect(400)
+            .end(done);
+    });
+});
+
+describe('GET /api/user/:id/todos', () => {
+    it('should get all todos of a specific user', (done) => {
+        request(app)
+            .get(`/api/user/${userID}/todos`)
+            .expect(200)
+            .end(done);
+    });
+});
+
+describe('DELETE /api/user/:id/todos/:todo_id', () => {
+    let textToDelete = "Learn Mocha"
+    beforeEach(async () => {
+        const userID = "5b7ec61bdfe1d02eb401c7a9";
+        const createdTodo = await ToDoDB.create({ text: textToDelete });
+        todoID = createdTodo.id;
+        const foundUser = await User.findOne({ _id: userID });
+        foundUser.todos.push(createdTodo.id);
+        await foundUser.save();
+    });
+
+    it('should remove a todo', (done) => {
+        request(app)
+            .delete(`/api/user/${userID}/todos/${todoID}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.deletedTodo.text).toBe(textToDelete);
+            })
             .end((err, res) => {
                 if (err) {
                     return done(err);
                 }
-                ToDoDB.find({}).then((todos) => {
-                    expect(todos.length).toBe(2);
+                ToDoDB.findById(todoID).then((todo) => {
+                    expect(todo).toBeNull();
                     done();
                 })
                     .catch((err) => done(err));
             });
     });
-});
 
-describe('GET /api/todos', () => {
-    it('should get all todos from the the DB', (done) => {
-        request(app)
-            .get('/api/todos')
-            .expect(200)
-            .expect((res) => {
-                expect(res.body.todos.length).toBe(2);
-            })
-            .end(done);
-    });
-});
-
-describe('GET /api/todos/:id', () => {
-    it('should return a ToDo', (done) => {
-        const { _id, text } = seedToDos[0];
-        request(app)
-            .get(`/api/todos/${_id}`)
-            .expect(200)
-            .expect((res) => {
-                expect(res.body.todo.text).toBe(text);
-            })
-            .end(done);
-    });
     it('should return 404 if todo not found', (done) => {
         const id = new ObjectID();
         request(app)
-            .get(`/api/todos/${id}`)
+            .delete(`/api/user/${userID}/todos/${id}`)
             .expect(404)
             .end(done);
     });
+
     it('should return 404 for invalid ID', (done) => {
-       request(app)
-       .get('/api/todos/123')
-       .expect(404)
-       .end(done);
+        request(app)
+            .delete(`/api/user/${userID}/todos/123`)
+            .expect(404)
+            .end(done);
     });
 });
 
-describe('DELETE /api/todos/:id', () => {
-     it('should remove a todo', (done) => {
-        const { _id, text } = seedToDos[0];
-        request(app)
-        .delete(`/api/todos/${_id}`)
-        .expect(200)
-        .expect((res) => {
-            expect(res.body.deletedTodo.text).toBe(text);
-        })
-        .end((err, res) => {
-            if(err){
-                return done(err);
-            }
-            ToDoDB.findById(_id).then((todo) => {
-                expect(todo).toBeNull();
-                done();
-            })
-            .catch((err) => done(err));
-        });
-     });
-
-     it('should return 404 if todo not found', (done) => {
-        const id = new ObjectID();
-        request(app)
-            .delete(`/api/todos/${id}`)
-            .expect(404)
-            .end(done);
-     });
-
-     it('should return 404 for invalid ID', (done) => {
-        request(app)
-        .delete('/api/todos/123')
-        .expect(404)
-        .end(done);
-     });
-});
-
-describe('PUT /api/todos/:id', () => {
+describe('PUT /api/user/:id/todos/:todo_id', () => {
     it('should update a todo', (done) => {
-       const { _id} = seedToDos[1];
        request(app)
-       .put(`/api/todos/${_id}`)
-       .send({text: "Learn German", completed: true})
+       .put(`/api/user/${userID}/todos/${todoID}`)
+       .send({completed: true})
        .expect(200)
        .expect((res) => {
-           expect(res.body.updatedTodo._id).toBe(_id.toHexString());
+           expect(res.body.updatedTodo._id).toBe(todoID);
        })
        .end((err, res) => {
            if(err){
                return done(err);
            }
-           ToDoDB.findById(_id).then((todo) => {
+           ToDoDB.findById(todoID).then((todo) => {
                expect(todo.completed).toBeTruthy();
-               expect(todo.text).toBe("Learn German");
+               expect(todo.text).toBe("Learn Mocha");
                done();
            })
            .catch((err) => done(err));
@@ -164,19 +109,17 @@ describe('PUT /api/todos/:id', () => {
     });
 
     it('should return 404 if todo not found', (done) => {
-       const id = new ObjectID();
-       request(app)
-           .put(`/api/todos/${id}`)
-           .send({text: "Learn German", completed: true})
-           .expect(404)
-           .end(done);
+        const id = new ObjectID();
+        request(app)
+            .put(`/api/user/${userID}/todos/${id}`)
+            .expect(404)
+            .end(done);
     });
 
     it('should return 404 for invalid ID', (done) => {
-       request(app)
-       .put('/api/todos/123')
-       .send({text: "Learn German", completed: true})
-       .expect(404)
-       .end(done);
+        request(app)
+            .put(`/api/user/${userID}/todos/123`)
+            .expect(404)
+            .end(done);
     });
 });
